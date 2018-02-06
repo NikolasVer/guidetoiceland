aac6a01be7f2c43fef012bf38c3d31c1e099016b
Nikolay <nikolay.veretelnik@codeit.com.ua>
2018-02-06 19:54:33 +0200

    add tests

--- client/src/components/Nav/index.test.js
+++ client/src/components/Nav/index.test.js
@@ -11,26 +11,36 @@ describe("Render tests", () => {
     expect(component).toBeDefined();
   });
 
-
-  it('should render 4 buttons', () => {
-    const buttons = component.find('.btn');
+  it("should render 4 buttons", () => {
+    const buttons = component.find(".btn");
     expect(buttons).toHaveLength(4);
   });
 
-
   it('each button should have "btn" class', () => {
-    component.find('button').forEach(node => {
-      expect(node.hasClass('btn')).toBeTruthy();
-    })
+    component.find("button").forEach(node => {
+      expect(node.hasClass("btn")).toBeTruthy();
+    });
   });
-
 
   it('active button should have "active" class', () => {
     const activeID = component.state().active;
     const activeBtn = component.find(`.btn`).get(activeID);
-    expect(shallow(activeBtn).hasClass('active')).toEqual(true);
+    expect(shallow(activeBtn).hasClass("active")).toEqual(true);
   });
+});
 
+it('should called "clickHandler" on button click', () => {
+  const fn = jest.fn();
+  const component = shallow(<NavButtons _changePhotoCount={fn} />);
+  const button = shallow(component.find(".btn").get(3));
+
+  const spy = jest.spyOn(component.instance(), "clickHandler");
+
+  component.update();
+  button.simulate("click");
+
+  expect(spy).toHaveBeenCalled();
+  expect(spy.mock.calls[0]).toEqual([3, 30]);
 });
 
 describe("State tests", () => {
@@ -40,13 +50,12 @@ describe("State tests", () => {
     expect(component.state().active).toEqual(0);
   });
 
-
   it("state should change on button click", () => {
     const newActiveID = 3;
-    const button = shallow(component.find('.btn').get(newActiveID));
+    const button = shallow(component.find(".btn").get(newActiveID));
 
-    button.simulate('click');
+    button.simulate("click");
 
-    expect(component.state().active).toEqual(newActiveID)
+    expect(component.state().active).toEqual(newActiveID);
   });
 });
--- client/src/components/Photos/index.test.js
+++ client/src/components/Photos/index.test.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
 import "../../setupTests";
+import { shallow, mount } from "enzyme";
 import PhotosSection from "./index";
 import { ApolloProvider } from "react-apollo";
-import createNewClient from '../../graphql/client';
+import createNewClient from "../../graphql/client";
+import { MockedProvider } from "react-apollo/test-utils";
 
-describe('Render tests', () => {
-  const wrapper = shallow(
-    <ApolloProvider client={createNewClient()} >
+describe("PhotosSection tests", () => {
+  const wrapper = mount(
+    // not shallow
+    <MockedProvider client={createNewClient()}>
       <PhotosSection count={10} />
-    </ApolloProvider>
+    </MockedProvider>
   );
 
-  it('render wihout crashing', () => {
-    expect(wrapper.find(PhotosSection)).toHaveLength(1);
+  const component = wrapper.find(PhotosSection);
+  it("render wihout crashing", () => {
+    expect(component).toBeDefined();
+  });
+
+  it("should have count props", () => {
+    expect(component.props().count).toBeTruthy();
   });
 });
